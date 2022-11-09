29f201b1ae301462e7f38d7eb721cb46f01ff032
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-09 19:27:09 +0100

    Add static mobile header

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,44 @@ module.exports = {
     author: `gedankenessen`,
     url: `https://gedankenessen.de`,
     card: `/card.png`,
+    color: `#D9A621`,
   },
   pathPrefix: '/',
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`],
+          placeholder: `none`,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `#D9A621`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        }
+      }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        }
+      }
+    },
+  ],
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../../assets/logo-type-b.svg';
 import './Header.css';
 
 export const Header = () => (
   <header className="header-container">
-    <p className="header-text">gedankenessen</p>
+    <Logo className="header-logo" alt="Logo" />
     <div className="burger-menu-container">
       <span className="burger-menu-item" />
       <span className="burger-menu-item" />
