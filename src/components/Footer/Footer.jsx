fab9f710ddc2671c05ea3f12304f353ee9fc4832
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-09 20:15:01 +0100

    Cleanup Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,25 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import Logo from '../../assets/logo-type-a.svg';
 import './Footer.css';
 
 export const Footer = () => (
   <footer className="footer-container">
-    <p className="footer-logo">gedankenessen</p>
+    <Logo className="footer-logo" />
     <div className="footer-access-container">
-      <Link to='/login'><p className="footer-text">Login</p></Link>
-      <Link to='/login'><p className="footer-text">Register</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Login</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Register</p></Link>
     </div>
     <div className="footer-interest-container">
-      <Link to='/login'><p className="footer-text">Pricing</p></Link>
-      <Link to='/login'><p className="footer-text">Features</p></Link>
-      <Link to='/login'><p className="footer-text">FAQ</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Pricing</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Features</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">FAQ</p></Link>
     </div>
     <div className="footer-service-container">
-      <Link to='/login'><p className="footer-text">Blog</p></Link>
-      <Link to='/login'><p className="footer-text">Privacy</p></Link>
-      <Link to='/login'><p className="footer-text">Terms</p></Link>
-      <Link to='/login'><p className="footer-text">Contact</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Blog</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Privacy</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Terms</p></Link>
+      <Link to='/login' className="footer-link"><p className="footer-text">Contact</p></Link>
     </div>
   </footer>
 )
