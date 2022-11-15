863297eba9e9e5d073345e6fe0c700a78fdf0667
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-15 18:11:48 +0100

    Fix demo spacing

--- src/components/About/Demos/Demo.jsx
+++ src/components/About/Demos/Demo.jsx
@@ -3,7 +3,11 @@ import './Demo.css';
 
 export const Demo = ({ text, code }) => (
   <div className="demo-wrapper">
-    {text}
-    {code}
+    <div className="demo-text">
+      {text}
+    </div>
+    <div className="demo-code">
+      {code}
+    </div>
   </div>
 )
