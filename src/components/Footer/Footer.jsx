f5eb501f6f69535d1110e740ccba21cac67d5f28
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-15 20:00:42 +0100

    Fix footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,18 +10,18 @@ export const Footer = () => (
     </Link>
     <div className="footer-access-container">
       <Link to='/login' className="footer-link"><p className="footer-text">Login</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">Register</p></Link>
+      <Link to='/register' className="footer-link"><p className="footer-text">Register</p></Link>
     </div>
     <div className="footer-interest-container">
-      <Link to='/login' className="footer-link"><p className="footer-text">Pricing</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">Features</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">FAQ</p></Link>
+      <Link to='/pricing' className="footer-link"><p className="footer-text">Pricing</p></Link>
+      <Link to='/features' className="footer-link"><p className="footer-text">Features</p></Link>
+      <Link to='/faq' className="footer-link"><p className="footer-text">FAQ</p></Link>
     </div>
     <div className="footer-service-container">
-      <Link to='/login' className="footer-link"><p className="footer-text">Blog</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">Privacy</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">Terms</p></Link>
-      <Link to='/login' className="footer-link"><p className="footer-text">Contact</p></Link>
+      <Link to='/blog' className="footer-link"><p className="footer-text">Blog</p></Link>
+      <Link to='/privacy' className="footer-link"><p className="footer-text">Privacy</p></Link>
+      <Link to='/terms' className="footer-link"><p className="footer-text">Terms</p></Link>
+      <Link to='/contact' className="footer-link"><p className="footer-text">Contact</p></Link>
     </div>
   </footer>
 )
