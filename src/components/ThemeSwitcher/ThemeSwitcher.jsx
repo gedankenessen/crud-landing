442d8faa7ce173b37b31ba2e35d0f386727fa1fc
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-16 10:52:05 +0100

    Make theme switcher appear clickable

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -7,7 +7,7 @@ export const ThemeSwitcher = () => {
   return (
     <div className="theme-switch-wrapper">
       <button className="theme-switch-button" onClick={toggleTheme}>
-        <div className="theme-switch-icon" style={{ backgroundColor: theme.color }} />
+        <div className="theme-switch-icon" style={{ backgroundColor: theme.color, cursor: 'pointer' }} />
       </button>
     </div>
   )
