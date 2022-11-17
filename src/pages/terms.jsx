93a06e29ad896dcdb132ca323720a73566a8c601
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-17 14:22:11 +0100

    Add terms skeleton page

---
+++ src/pages/terms.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Layout } from '../components/Layout/Layout';
+import { Seo } from '../components/Seo/Seo';
+
+const Terms = () => (
+  <Layout>
+    <h1>Terms</h1>
+  </Layout>
+)
+
+export const Head = () => <Seo />;
+
+export default Terms;
