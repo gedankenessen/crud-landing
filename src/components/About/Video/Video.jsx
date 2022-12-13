e8536c2408b660232c744bb70bc9bffe3495469c
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-13 15:58:28 +0100

    Add ugly placeholder to video on landing

--- src/components/About/Video/Video.jsx
+++ src/components/About/Video/Video.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import './Video.css';
 
 export const Video = () => (
-  <div className="video-wrapper"></div>
+  <div className="video-wrapper">
+    <div className="video-placeholder-wrapper">
+    <div className="video-placeholder-container">
+      <p className="video-placeholder-text">More</p>
+      <p className="video-placeholder-text">coming</p>
+      <p className="video-placeholder-text">soon.</p>
+    </div>
+    </div>
+  </div>
 )
