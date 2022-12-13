67c156fc6de4f428551e42a9d17cf395ff89a541
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-13 15:58:21 +0100

    Cleanup pages

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { Layout } from '../components/Layout/Layout';
 import { Seo } from '../components/Seo/Seo';
 
 const Contact = () => (
-  <Layout>
+  <Layout current='Contact'>
     <h1>Contact</h1>
+    <h3>email:</h3>
+    <h3><Link to={'mailto:team@gedankenessen.de'} target='_blank'>team@gedankenessen.de</Link></h3>
+    <h3>website:</h3>
+    <h3><Link to={'https://www.gedankenessen.de'} target='_blank'>www.gedankenessen.de</Link></h3>
+    <h3>github:</h3>
+    <h3><Link to={'https://www.github.com/gedankenessen'} target='_blank'>www.github.com/gedankenessen</Link></h3>
   </Layout>
 )
 
-export const Head = () => <Seo />;
+export const Head = () => <Seo title='Contact' />;
 
 export default Contact;
