12652b5e327606b134a1be3ca376446e0f0707c3
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-12-17 20:09:04 +0100

    Fix typos

--- src/components/About/Features/Features.jsx
+++ src/components/About/Features/Features.jsx
@@ -8,20 +8,18 @@ const features = [
     description: <p>
       Easily create a full CRUD endpoint by POSTing your JSON to crud.
       Query, add, delete and iterate your data - all by using HTTP methods.
-      crud takes of the CRUD and enables you to validate ideas in hours - instead of days
     </p>,
   },
   {
     title: <h2>Don't mock data, <span className="highlight">prototype</span> it</h2>,
     description: <p>
-      Don’t implement twice by instead getting your data from a backend right from the start.
+      Don’t implement twice - get your data from a backend right from the start.
       No need to add state transition wiring after the fact - design with it in mind.
     </p>,
   },
   {
     title: <h2><span className="highlight">Share</span> your progress easily</h2>,
     description: <p>No need for complicated deploy strategies. Everything is online and ready for you and your team.</p>,
-
   },
   {
     title: <h2>And <span className="highlight">more</span> coming soon</h2>,
